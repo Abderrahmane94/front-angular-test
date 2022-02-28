ac5122eb4db04ce8452bd330aa49ef94d3639ece
Abderrahmane <abderrahmanesmailon20@gmail.com>
2022-02-28 01:47:47 +0100

    - Add login && register component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,36 @@
 import {Component} from '@angular/core';
+import {TokenStorageService} from "./_services/token-storage.service";
 
 
 @Component({
   selector: 'app-root',
-  template: `
-    <mat-toolbar class="mat-elevation-z6" color="primary">
-      <span>{{pageTitle}}</span>
-      <a mat-button routerLink="/welcome">Home</a>
-      <a mat-raised-button color="warn" routerLink="/products">Product List</a>
-      <a mat-raised-button color="accent" class="m-2" routerLink="/register">Register</a>
-    </mat-toolbar>
-    <div class="container m-5">
-      <router-outlet></router-outlet>
-    </div>
-  `,
+  template: './app.component.html',
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   pageTitle = 'Product management APP';
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) {
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,18 @@ import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {WelcomeComponent} from './home/welcome/welcome.component';
-import {RouterModule} from "@angular/router";
 import {ProductModule} from './products/product.module';
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardUserComponent } from './board-user/board-user.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
-import { authInterceptorProviders } from './_helpers/auth.interceptor';
-
-
-
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
+import {BoardAdminComponent} from './board-admin/board-admin.component';
+import {BoardUserComponent} from './board-user/board-user.component';
+import {BoardModeratorComponent} from './board-moderator/board-moderator.component';
+import {authInterceptorProviders} from './_helpers/auth.interceptor';
+import {AppRoutingModule} from "./app-routing.module";
 
 
 @NgModule({
@@ -38,12 +35,13 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'welcome', component: WelcomeComponent},
-      {path: 'register', component: RegisterComponent},
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      {path: '**', redirectTo: 'welcome', pathMatch: 'full'} //it's useful for 404 page
-    ]),
+    AppRoutingModule,
+    /*    RouterModule.forRoot([
+          {path: 'welcome', component: WelcomeComponent},
+          {path: 'register', component: RegisterComponent},
+          {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+          {path: '**', redirectTo: 'welcome', pathMatch: 'full'} //it's useful for 404 page
+        ]),*/
     ProductModule,
     MatButtonModule,
     MatToolbarModule,
